2ad008d270d9f094953cd03128fb91f6714163e2
Shakil4432 <shakil4432@gmail.com>
2024-12-28 01:16:52 +0600

    handle error

--- Library-Management-System/src/modules/books/books.controller.ts
+++ Library-Management-System/src/modules/books/books.controller.ts
@@ -1,47 +1,31 @@
 import { Request, Response } from "express";
 import { bookValidation } from "./books.validation";
 import { BookServices } from "./books.services";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
-const createBook = async (req: Request, res: Response) => {
-  try {
-    const bookData = req.body;
+const createBook = catchAsync(async (req: Request, res: Response) => {
+  const bookData = req.body;
+  const zodBookParseData = bookValidation.bookValidationSchema.parse(bookData);
+  const result = await BookServices.createBookIntoDB(zodBookParseData);
 
-    const zodBookParseData =
-      bookValidation.bookValidationSchema.parse(bookData);
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Book created successfully",
+    data: result,
+  });
+});
 
-    const result = await BookServices.createBookIntoDB(zodBookParseData);
-
-    res.status(200).json({
-      success: true,
-      message: "Book created successfully",
-      data: result,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "An error occurred while creating the book",
-      error: error,
-    });
-  }
-};
-
-const borrowBook = async (req: Request, res: Response) => {
+const borrowBook = catchAsync(async (req: Request, res: Response) => {
   const bookId = req.params.id;
-  try {
-    const result = await BookServices.borrowBookFromDB(bookId);
-
-    res.status(200).json({
-      success: true,
-      message: "Book borrowed successfully",
-      data: result,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: error,
-    });
-  }
-};
+  const result = await BookServices.borrowBookFromDB(bookId);
+  res.status(200).json({
+    success: true,
+    message: "Book borrowed successfully",
+    data: result,
+  });
+});
 
 export const BookControllers = {
   createBook,
